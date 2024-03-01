7304e372e2fe1dea1e9974cb85c8fdbd46fc3c37
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2024-03-01 17:13:39 -0400

    Feat - implementa layout inicial e navegação

--- app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.marvelapp.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.marvelapp.R
+import com.example.marvelapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+
+        navController = navHostFragment.navController
+        binding.navView.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.home, R.id.favorite, R.id.about
+            )
+        )
+
     }
 }
