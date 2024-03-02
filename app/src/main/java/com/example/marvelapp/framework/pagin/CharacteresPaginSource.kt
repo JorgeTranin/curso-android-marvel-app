7995dd5d37b381644be5aaa9267742e3a339c292
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2024-03-02 16:36:57 -0400

    Feat - implementa CharacteresPaginSource

---
+++ app/src/main/java/com/example/marvelapp/framework/pagin/CharacteresPaginSource.kt
@@ -0,0 +1,54 @@
+package com.example.marvelapp.framework.pagin
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.marvelapp.framework.response.DataWrapperResponse
+import com.example.marvelapp.framework.response.toCharacterModel
+import com.jorge.core.data.repository.CharacterRemoteDataSource
+import com.jorge.core.domain.models.Character
+
+class CharacteresPaginSource(
+    private val remoteDataSource: CharacterRemoteDataSource<DataWrapperResponse>,
+    private val query: String
+) : PagingSource<Int, Character>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Character> {
+       return try {
+            val offset = params.key ?: 0
+
+            val queries = hashMapOf(
+                "offset" to offset.toString()
+            )
+            if (query.isNotEmpty()) {
+                queries["nameStartWith"] = query
+            }
+
+            val response = remoteDataSource.fetchCharacter(queries)
+            val responseOffSet = response.dataContainerResponse.offset
+            val limit = response.dataContainerResponse.total
+
+            LoadResult.Page(
+                data = response.dataContainerResponse.characterResponses.map { it.toCharacterModel() },
+                prevKey = null,
+                nextKey = if (responseOffSet < limit) {
+                    responseOffSet + LIMIT
+                } else null
+            )
+
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Character>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?. prevKey?.plus(LIMIT) ?: anchorPage?.nextKey?.minus(LIMIT)
+
+        }
+    }
+
+    companion object {
+        private const val LIMIT = 20
+    }
+}
--- app/src/main/java/com/example/marvelapp/framework/response/CharacterResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/response/CharacterResponse.kt
@@ -1,5 +1,7 @@
 package com.example.marvelapp.framework.response
 
+import com.jorge.core.domain.models.Character
+
 data class CharacterResponse(
     val description: String,
     val id: Int,
@@ -8,3 +10,9 @@ data class CharacterResponse(
     val resourceURI: String,
     val thumbnailRespose: ThumbnailRespose
 )
+fun  CharacterResponse.toCharacterModel(): Character{
+    return Character(
+        name = this.name,
+        imageUrl = "${this.thumbnailRespose.path}.${this.thumbnailRespose.extension}"
+    )
+}
