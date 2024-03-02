cf5cbe128194c821582faaeb2234f8a7a1dc413e
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2024-03-02 16:42:17 -0400

    Feat - implementa RepositoryModule

--- app/src/main/java/com/example/marvelapp/framework/CharacterRepositoryImpl.kt
+++ app/src/main/java/com/example/marvelapp/framework/CharacterRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.example.marvelapp.framework
 
 import androidx.paging.PagingSource
+import com.example.marvelapp.framework.pagin.CharacteresPaginSource
 import com.example.marvelapp.framework.response.DataWrapperResponse
 import com.jorge.core.data.repository.CharacterRemoteDataSource
 import com.jorge.core.data.repository.CharacterRepository
@@ -11,6 +12,6 @@ class CharacterRepositoryImpl @Inject constructor(
     private val remoteDataSource: CharacterRemoteDataSource<DataWrapperResponse>
 ): CharacterRepository {
     override fun getCharacter(query: String): PagingSource<Int, Character> {
-
+        return CharacteresPaginSource(remoteDataSource, query)
     }
 }
