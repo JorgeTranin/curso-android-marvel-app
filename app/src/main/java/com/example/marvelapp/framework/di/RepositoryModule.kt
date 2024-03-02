f9a494989cfdfd1db2c7cf8f976bf31f62aef004
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2024-03-02 16:45:52 -0400

    Feat - implementa RepositoryModule

--- app/src/main/java/com/example/marvelapp/framework/di/RepositoryModule.kt
+++ app/src/main/java/com/example/marvelapp/framework/di/RepositoryModule.kt
@@ -1,6 +1,9 @@
 package com.example.marvelapp.framework.di
 
 import com.example.marvelapp.framework.CharacterRepositoryImpl
+import com.example.marvelapp.framework.remote.RetrofitCharacterDataSource
+import com.example.marvelapp.framework.response.DataWrapperResponse
+import com.jorge.core.data.repository.CharacterRemoteDataSource
 import com.jorge.core.data.repository.CharacterRepository
 import dagger.Binds
 import dagger.Module
@@ -12,4 +15,10 @@ import dagger.hilt.components.SingletonComponent
 interface RepositoryModule {
     @Binds
     fun bindCharacterRepository(repositoryImpl: CharacterRepositoryImpl): CharacterRepository
+
+    @Binds
+    fun bindRemoteDataSource(dataSource: RetrofitCharacterDataSource
+    ): CharacterRemoteDataSource<DataWrapperResponse>
+
+
 }
