7a0dd63b63e18a0099b4186ff4b4509ac2651a00
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2024-03-22 12:43:01 -0400

    Feat - implementa layout de erro e impl de estados

--- app/src/main/java/com/example/marvelapp/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/home/HomeFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import com.example.marvelapp.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
@@ -35,20 +37,53 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initAdapterCharacters()
+        observerInitalState()
+        initData()
+    }
 
+
+
+    private fun initData() {
         lifecycleScope.launch {
             homeViewModel.getCharacterPaginData("").collectLatest { pagingData ->
                 charactersAdapter.submitData(pagingData)
             }
         }
-
     }
 
     private fun initAdapterCharacters() {
-        charactersAdapter = CharactersAdapter()
+
         binding.recyclerCharacters.run {
+            charactersAdapter = CharactersAdapter()
             setHasFixedSize(true)
             adapter = charactersAdapter
+
+        }
+    }
+
+    private fun observerInitalState() {
+        lifecycleScope.launch {
+            charactersAdapter.loadStateFlow.collectLatest { state ->
+                binding.flipperCharacters.displayedChild = when (state.refresh) {
+                    is LoadState.Loading -> {
+                        setShimmerVisibility(true)
+                        FLIPER_LOADING
+                    }
+
+                    is LoadState.NotLoading -> {
+                        setShimmerVisibility(false)
+                        FLIPPER_SUCCESS
+                    }
+
+                    is LoadState.Error -> {
+                        binding.includeViewCharacterErrorState.btnRetry.setOnClickListener {
+                            charactersAdapter.retry()
+                        }
+                        FLIPER_ERROR
+                    }
+                }
+
+            }
 
         }
     }
@@ -57,5 +92,23 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setShimmerVisibility(visibility: Boolean) {
+        binding.includeViewCharacterLoadingState.shimmerCharacter.run {
+            isVisible = visibility
+            if (visibility) {
+                startShimmer()
+            } else {
+                stopShimmer()
+            }
+        }
+
+    }
+
+    companion object {
+        private const val FLIPER_LOADING = 0
+        private const val FLIPPER_SUCCESS = 1
+        private const val FLIPER_ERROR = 2
     }
 }
